2a2f6034d8430313bc9ed3b281e97d898aa40c52
Mathhew Tejo <matthew.tejo@lookout.com>
2015-09-26 12:07:38 -0700

    Consul get current and update connections work

--- core/src/main/scala/com/lookout/borderpatrol/sessionx/SecretStores.scala
+++ core/src/main/scala/com/lookout/borderpatrol/sessionx/SecretStores.scala
@@ -4,6 +4,8 @@ import org.jboss.netty.handler.codec.http.{DefaultHttpRequest, HttpRequest, Http
 import com.twitter.finagle.Service
 import com.twitter.finagle.builder.ClientBuilder
 import com.twitter.finagle.http._
+import com.twitter.io.Charsets
+import org.jboss.netty.buffer.ChannelBuffers
 
 
 /**
@@ -58,6 +60,15 @@ object SecretStores {
     
     
     def current: Secret = {
+      val req = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/v1/kv/?recurse")
+      val f = consul(req) // Client, send the request
+
+    // Handle the response:
+      f onSuccess { res =>
+       println("got response" + res.getContent.toString(Charsets.Utf8) )
+      } onFailure { exc =>
+        println("failed :-(" + exc)
+      }
       Secret()
     }
 
@@ -70,19 +81,30 @@ object SecretStores {
     }
 
     def update(newSecret: Secret): Boolean ={
+       val data = ChannelBuffers.copiedBuffer("String from borderpatrol", Charsets.Utf8)
+      val req: HttpRequest= RequestBuilder()
+        .url("http://localhost:8500/v1/kv/web/key2")
+        .buildPut(data)
+      //val req = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/v1/kv/secretStore/current")
+    
+     val f = consul(req)
+      f onSuccess { res =>
+       println("got response" + res.getContent.toString(Charsets.Utf8) )
+      } onFailure { exc =>
+        println("failed :-(" + exc)
+      }
       true
-
     }
   }
 
   object ConsulSecretStore{
 
     def apply(consulUrl: String, consulPort: String):ConsulSecretStore = {
-      var apiUrl = s"http://$consulUrl:$consulPort/v1/kv/secretStore/"
+      var apiUrl = s"$consulUrl:$consulPort"
       println(apiUrl)
       val client: Service[HttpRequest, HttpResponse] = ClientBuilder()
         .codec(Http())
-        .hosts(apiUrl) // If >1 host, client does simple load-balancing
+        .hosts(apiUrl) 
         .hostConnectionLimit(5)
         .build()
 
